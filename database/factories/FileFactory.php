21c1729e0a50ee26462a8301dc3ce828ef7b3a92
宮良一生 <issei0804@outlook.com>
2023-05-15 23:00:53 +0900

    File model を追加

---
+++ database/factories/FileFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Exception;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\File>
+ */
+class FileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * @throws Exception
+     */
+    public function definition(): array
+    {
+        $file_name = $this->faker->word;
+        $file_path = $this->faker->word;
+        $count = 0;
+        while (Storage::exists($file_path)) {
+            $file_path = $this->faker->word;
+            $count++;
+            if ($count > 10) {
+                // throw error
+                throw new Exception('FileFactory definition() failed');
+            }
+        }
+        Storage::put($file_path, $this->faker->text);
+        return [
+            'file_name' => $file_name,
+            'file_path' => $file_path,
+        ];
+    }
+}
