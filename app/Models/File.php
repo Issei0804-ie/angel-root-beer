3a46d21e9317ec2a69809d16efa6647854091077
宮良一生 <issei0804@outlook.com>
2023-05-15 23:02:01 +0900

    laravel-ide-helperを導入

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\File
+ *
+ * @method static \Database\Factories\FileFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|File newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|File newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|File query()
+ * @mixin \Eloquent
+ */
 class File extends Model
 {
     use HasFactory;
