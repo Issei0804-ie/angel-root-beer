2367ee9c35b82fd450ddfb5f141b6effb2d73c3c
宮良一生 <issei0804@outlook.com>
2023-05-16 20:50:30 +0900

    Seederのリファクタ

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'seed' => [
+            'driver' => 'local',
+            'root' => storage_path('app/for-seed-data'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
